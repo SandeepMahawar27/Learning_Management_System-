1bda978ac719372ed0e807b9c0b92fe59b33db00
SANDEEEP259 <80709077+SANDEEEP259@users.noreply.github.com>
2024-03-12 12:21:41 +0530

    Final Project

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use( 
 	cors({
-		origin: ["deployed-vercel-frontend-app","localhost:3000"],
+		origin: ["https://deploy-mern-1whq.vercel.app"],
         methods: ["POST", "GET"], 
         credentials: true
 	})
